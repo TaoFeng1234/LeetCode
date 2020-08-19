0a489ccd87dc64279c0c83610b71a4464c7cdde1
TaoFeng1234 <56402631+TaoFeng1234@users.noreply.github.com>
2020-08-19 16:08:45 -0400

    Update Two_Sum.py

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,6 +1,5 @@
 #If the Array is sorted
 
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         #sort the array
@@ -16,3 +15,15 @@ class Solution:
                 else: 
                     j-=1
         return i,j
+
+# If the array is unsorted. Solution provided by leetcode user/HankLiu5
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n],i]
