28cd78c5a37511524c8f93b20df6624b37e4f319
TaoFeng1234 <56402631+TaoFeng1234@users.noreply.github.com>
2020-08-19 16:09:11 -0400

    Update Two_Sum.py

--- Two_Sum.py
+++ Two_Sum.py
@@ -3,7 +3,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         #sort the array
-        nums.sort()
         i = 0
         j = len(nums)-1
         while i<j :
